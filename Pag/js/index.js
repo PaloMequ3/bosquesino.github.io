ebc9010763da2d38e03fa2df4ce7bd26d1268eeb
PaloMequ3 <unicocamiloenelmundo@gmail.com>
2024-05-15 19:09:35 -0500

    Add files via upload

---
+++ Pag/js/index.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.header', {
+        duration: 2000,
+});
+
+sr.reveal('.busquesino', {
+    duration: 2000,
+    delay: 1000
+});
+sr.reveal('.txt',{
+    duration:3000,
+    origin:'bottom',
+    delay: 1000,
+});
+/*
+sr.reveal('.box-1',{
+    duration:3000,
+    origin:'top',
+    delay: 500,
+    distance:'-100px'
+})*/
+sr.reveal('.txt-2',{
+    duration: 3000,
+    origin:'bottom',
+    delay:1000,
+    distance: '-100px',
+});
+
+
+
+
+
